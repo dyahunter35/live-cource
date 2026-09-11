69d6e86a25b81c3cab84718c30d4f47650d39e74
Dya bushra Mohammed <dyahunter35@gmail.com>
2026-09-11 18:01:37 +0200

    feat: add live courses route and navigation items to header and sidebar

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
-Route::livewire('/live-courses', LiveCoursesIndex::class)->name('live-courses');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::livewire('/live-courses', LiveCoursesIndex::class)->name('live-courses');
+
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
